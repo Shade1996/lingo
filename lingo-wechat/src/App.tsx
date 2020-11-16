2a439b295591dec0b520e792d81bcae97d7e355f
Shade1996 <leizhiheng10@gmail.com>
2020-11-16 21:18:02 +0800

    20201116 21:17

--- lingo-wechat/src/App.tsx
+++ lingo-wechat/src/App.tsx
@@ -3,7 +3,7 @@ import WelcomePage from "./components/WelcomePage";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core";
 import { blue } from "@material-ui/core/colors";
 import DemoPage from "./components/DemoPage";
-import TotalLessonPage from "./components/TotalLessonPage";
+import TotalItemPage from "./components/TotalItemPage";
 import LevelPage from "./components/LevelPage";
 import { useCurrentPage } from "./state";
 
@@ -18,29 +18,29 @@ const App = () => {
     const [dialogOpen, setDialogOpen] = React.useState(false);
     const [currentPage] = useCurrentPage()
 
-    if (currentPage === "welcome")
-        return <ThemeProvider theme={theme}>
-                <WelcomePage />
-                </ThemeProvider>
+    // if (currentPage === "welcome")
+    //     return <ThemeProvider theme={theme}>
+    //             <WelcomePage />
+    //             </ThemeProvider>
 
-    if (currentPage === "level")
-        return <ThemeProvider theme={theme}>
-                <LevelPage />
-                </ThemeProvider>
+    // if (currentPage === "level")
+    //     return <ThemeProvider theme={theme}>
+    //             <LevelPage />
+    //             </ThemeProvider>
 
-    else if (currentPage === "total")
-        return <ThemeProvider theme={theme}>
-                <TotalLessonPage />
-                </ThemeProvider>
-    else
-        return <div>page not found</div>
-    // return (
-    //     <ThemeProvider theme={theme}>
-    //         {/* <WelcomePage /> */}
-    //         {/* <DemoPage /> */}
-    //         {/* <TotalLessonPage /> */}
-    //         <LevelPage />
-    //     </ThemeProvider>
-    // )
+    // else if (currentPage === "total")
+    //     return <ThemeProvider theme={theme}>
+    //             <TotalItemPage />
+    //             </ThemeProvider>
+    // else
+    //     return <div>page not found</div>
+    return (
+        <ThemeProvider theme={theme}>
+            {/* <WelcomePage /> */}
+            {/* <DemoPage /> */}
+            <TotalItemPage />
+            {/* <LevelPage /> */}
+        </ThemeProvider>
+    )
 }
 export default App
