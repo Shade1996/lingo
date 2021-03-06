0f1a824ab502a87358546e1d483eab01c07bc65d
Shade1996 <leizhiheng10@gmail.com>
2021-03-06 17:56:15 +0800

    20210306 17:56

--- lingo-search/src/App.tsx
+++ lingo-search/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { useTheme } from "@pinyinma/playground-theme"
+import "@pinyinma/playground-css"
 import { AppBar, InputBase, Paper, ThemeProvider, Toolbar } from "@material-ui/core"
 import SearchIcon from '@material-ui/icons/Search';
 import MediaGrid from "./components/MediaGrid";
 import SoundGrid from "./components/SoundGrid";
 import { useProxy } from "valtio";
 import { searchState } from "./state";
+import SoundCards from "./components/SoundCards";
 
 const App: React.FC = () => {
     const [theme] = useTheme()
@@ -13,24 +15,34 @@ const App: React.FC = () => {
     searchState.value="sound"
     return (
         <ThemeProvider theme={theme}>
-            <Paper className="h-screen w-screen absolute top-0 overflow-scroll overflow-x-hidden bg-gray-600" elevation={2} square={true}>
-                <AppBar color="default" position="sticky">
-                    <Toolbar>
-                        <div className="bg-gray-800 rounded-sm ml-4">
-                            <InputBase className="px-2"
-                            placeholder="Search…"
-                            />
+            <Paper className="h-screen w-screen absolute top-0 overflow-scroll overflow-x-hidden" elevation={0} square={true} style={{
+                backgroundColor: theme.customPalette.background.darker1
+            }}>
+                <AppBar position="sticky" elevation={0} className="bg-transparent bg-blur bg-black bg-opacity-25">
+                    <Toolbar variant="dense">
+                        <div className="rounded-sm ml-4 container flex items-center" style={{ background: theme.customPalette.background.lighter0 }}>
+                            <InputBase className="px-2 w-full" placeholder="Search…" />
                             <SearchIcon />
                         </div>
                     </Toolbar>
                 </AppBar>
-                    {(() => {
+                <div className="container ml-10">
+                    <div className="flex w-full gap-6">
+                        <div className="w-1/2 flex justify-center items-center py-6">
+                            <SoundCards />
+                        </div>
+                        <div className="w-1/2 flex justify-center items-center py-6">
+                            <SoundCards />
+                        </div>
+                    </div>
+                </div>
+                    {/* {(() => {
                         //@ts-ignore
                         if (searchState.value === "sound" )
                             return <SoundGrid />
                         if(searchState.value === "media")
                             return <MediaGrid />
-                    })()}
+                    })()} */}
             </Paper>
         </ThemeProvider>
     )
--- lingo-search/src/components/SoundCards.tsx
+++ lingo-search/src/components/SoundCards.tsx
@@ -1,16 +1,33 @@
-import { Card, CardActions } from '@material-ui/core'
+import { Card, CardActions, Fab } from '@material-ui/core'
 import React from 'react'
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import ShareIcon from '@material-ui/icons/Share';
 
 export default function SoundCards() {
     return (
-        <Card className="w-5/12 h-40 flex items-center px-4">
-            <div>
+        <Card className="w-full p-6 rounded-xl">
+            <div className="mb-4 text-lg opacity-75 ml-4">Hello World</div>
+            <div className="flex items-center gap-4 flex-wrap">
+                <audio controls />
+                <div className="flex-grow" />
+                <div className="flex gap-4">
+                    <Fab variant="extended" size="medium" color="secondary" className="opacity-75">
+                        <GetAppIcon className="mr-2" />
+                        下载
+                    </Fab>
+                    <Fab variant="extended" size="medium" color="primary" className="bg-white bg-opacity-25 opacity-75">
+                        <ShareIcon className="mr-2" />
+                        分享
+                    </Fab>
+                </div>
+            </div>
+            {/* <div>
                 <PlayCircleOutlineIcon fontSize="large"/>
             </div>
             <div className="ml-6">
                 props
-            </div>
+            </div> */}
         </Card>
     )
 }
